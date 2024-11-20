1a6bef43ffdaa126eec2dfab9492f532556e64e8
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-20 23:07:48 +0100

    Refactor MicroMenu button update logic for improved readability and maintainability

--- Modules/Menu.lua
+++ Modules/Menu.lua
@@ -1,22 +1,20 @@
 -- Update position and appearance of the MicroMenu buttons
 
-local function ButtonUpdate()
-    local frame = EnumerateFrames()
-    while frame do
-        local name = frame:GetName()
-        if name and name:find("MicroButton") then
-            frame:SetAlpha(0.5)
-            frame:SetScale(0.75)
-            frame:SetScript("OnEnter", nil)
-            frame:SetScript("OnLeave", nil)
+local function UpdateMicroMenuButtons()
+    local checkedFrames = EnumerateFrames()
+    while checkedFrames do
+        local microButtonFrame = checkedFrames:GetName()
+        if microButtonFrame and microButtonFrame:find("MicroButton") then
+            checkedFrames:SetAlpha(0.5)
+            checkedFrames:SetScale(0.75)
         end
-        frame = EnumerateFrames(frame)
+        checkedFrames = EnumerateFrames(checkedFrames)
     end
 
     CharacterMicroButton:ClearAllPoints()
     CharacterMicroButton:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 16, 0)
 end
 
-local MicroMenuEvents = CreateFrame("Frame")
-MicroMenuEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-MicroMenuEvents:SetScript("OnEvent", ButtonUpdate)
+local microMenuEventFrame = CreateFrame("Frame")
+microMenuEventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+microMenuEventFrame:SetScript("OnEvent", UpdateMicroMenuButtons)
