1838057d399a592799f1a7d05d08284c31ac5a15
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-20 22:21:58 +0100

    Refactor Minimap layout and appearance for improved usability and aesthetics

--- Modules/MiniMap.lua
+++ Modules/MiniMap.lua
@@ -1,46 +1,17 @@
--- Update position and appearance of the Minimap and its elements
-
-local MinimapContainerBackdrop = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
-MinimapContainerBackdrop:SetSize(144, 144)
-MinimapContainerBackdrop:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -16, -16)
-MinimapContainerBackdrop:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", edgeSize = 12})
-MinimapContainerBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-MinimapContainerBackdrop:SetFrameLevel(Minimap:GetFrameLevel() + 1)
-
 local function MinimapContainerUpdate()
     Minimap:SetClampedToScreen(false)
+    Minimap:SetParent(UIParent)
     Minimap:ClearAllPoints()
-    Minimap:SetPoint("TOPLEFT", MinimapContainerBackdrop, "TOPLEFT", 2, -2)
-    Minimap:SetPoint("BOTTOMRIGHT", MinimapContainerBackdrop, "BOTTOMRIGHT", -2, 2)
-    Minimap:SetMaskTexture("Interface/ChatFrame/ChatFrameBackground")
-
-    MinimapBorder:Hide()
-    MinimapBorderTop:Hide()
+    Minimap:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -16, -16)
     MinimapBackdrop:Hide()
-    MinimapToggleButton:Hide()
-    MinimapCompassTexture:SetTexture(nil)
     GameTimeFrame:Hide()
-    MinimapZoneTextButton:Hide()
+    MinimapCluster:Hide()
 end
 
 local MinimapContainerEvents = CreateFrame("Frame")
 MinimapContainerEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 MinimapContainerEvents:RegisterEvent("ZONE_CHANGED")
 MinimapContainerEvents:SetScript("OnEvent", MinimapContainerUpdate)
-
-
-
-
-function GetMinimapShape()
-    return "SQUARE"
-end
-
-local MinimapShapeEvents = CreateFrame("Frame")
-MinimapShapeEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-MinimapShapeEvents:SetScript("OnEvent", GetMinimapShape)
-
-
-
 
 local function MinimapScrollEnable(self, delta)
     if delta > 0 then
@@ -55,21 +26,18 @@ MinimapZoomEvents:SetAllPoints(Minimap)
 MinimapZoomEvents:EnableMouseWheel(true)
 MinimapZoomEvents:SetScript("OnMouseWheel", MinimapScrollEnable)
 
-
-
-
-local MinimapTimeBackdrop = CreateFrame("Frame", nil, MinimapContainerBackdrop, "BackdropTemplate")
+local MinimapTimeBackdrop = CreateFrame("Frame", nil, Minimap, "BackdropTemplate")
 MinimapTimeBackdrop:SetSize(48, 24)
-MinimapTimeBackdrop:SetPoint("CENTER", MinimapContainerBackdrop, "BOTTOM", 0, -2)
+MinimapTimeBackdrop:SetPoint("CENTER", Minimap, "BOTTOM", 0, -2)
 MinimapTimeBackdrop:SetBackdrop({
-    bgFile = "Interface/ChatFrame/ChatFrameBackground",
+    bgFile = "Interface/ChatFrame/ChatFrameBackground", -- Commented out to prevent black screen
     edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
-    tile = false, tileSize = 24, edgeSize = 12,
+    tile = true, tileSize = 24, edgeSize = 12,
     insets = {left = 2, right = 2, top = 2, bottom = 2}
 })
 MinimapTimeBackdrop:SetBackdropColor(0, 0, 0, 1)
 MinimapTimeBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-MinimapTimeBackdrop:SetFrameLevel(MinimapContainerBackdrop:GetFrameLevel() + 1)
+MinimapTimeBackdrop:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 
 local function MinimapTimeUpdate()
     for _, ButtonRegion in pairs({TimeManagerClockButton:GetRegions()}) do
@@ -77,13 +45,10 @@ local function MinimapTimeUpdate()
             ButtonRegion:Hide()
         end
     end
-
     TimeManagerClockButton:SetParent(MinimapTimeBackdrop)
     TimeManagerClockButton:SetAllPoints(MinimapTimeBackdrop)
-
     TimeManagerClockTicker:SetPoint("CENTER", TimeManagerClockButton, "CENTER", 0, 0)
     TimeManagerClockTicker:SetFont(STANDARD_TEXT_FONT, 12)
-
     TimeManagerFrame:ClearAllPoints()
     TimeManagerFrame:SetPoint("TOPRIGHT", MinimapTimeBackdrop, "BOTTOMRIGHT", 0, -4)
 end
@@ -91,9 +56,6 @@ end
 local MinimapTimeEvents = CreateFrame("Frame")
 MinimapTimeEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 MinimapTimeEvents:SetScript("OnEvent", MinimapTimeUpdate)
-
-
-
 
 local MinimapMailBackdrop = CreateFrame("Frame", nil, MiniMapMailFrame, "BackdropTemplate")
 MinimapMailBackdrop:SetPoint("TOPLEFT", MiniMapMailFrame, "TOPLEFT", -4, 4)
@@ -106,15 +68,14 @@ MinimapMailBackdrop:SetBackdrop({
 })
 MinimapMailBackdrop:SetBackdropColor(0, 0, 0, 1)
 MinimapMailBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-MinimapMailBackdrop:SetFrameLevel(MinimapContainerBackdrop:GetFrameLevel() + 1)
+MinimapMailBackdrop:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 
 local function MinimapMailUpdate()
     MiniMapMailBorder:Hide()
-    MiniMapMailFrame:SetParent(MinimapContainerBackdrop)
+    MiniMapMailFrame:SetParent(Minimap)
     MiniMapMailFrame:ClearAllPoints()
     MiniMapMailFrame:SetSize(16, 16)
-    MiniMapMailFrame:SetPoint("RIGHT", MinimapTimeBackdrop, "LEFT", -8, 0)
-
+    MiniMapMailFrame:SetPoint("RIGHT", MinimapTimeBackdrop, "LEFT", -4, 0)
     MiniMapMailIcon:ClearAllPoints()
     MiniMapMailIcon:SetSize(17, 17)
     MiniMapMailIcon:SetPoint("CENTER", MiniMapMailFrame, "CENTER", 0, 0)
@@ -124,31 +85,28 @@ local MinimapMailEvents = CreateFrame("Frame")
 MinimapMailEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 MinimapMailEvents:SetScript("OnEvent", MinimapMailUpdate)
 
-
-
-
 local MinimapBFBackdrop = CreateFrame("Frame", nil, MiniMapBattlefieldFrame, "BackdropTemplate")
 MinimapBFBackdrop:SetPoint("TOPLEFT", MiniMapBattlefieldFrame, "TOPLEFT", -4, 4)
 MinimapBFBackdrop:SetPoint("BOTTOMRIGHT", MiniMapBattlefieldFrame, "BOTTOMRIGHT", 4, -4)
 MinimapBFBackdrop:SetBackdrop({
     bgFile = "Interface/ChatFrame/ChatFrameBackground",
     edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
-    tile = false, tileSize = 16, edgeSize = 12,
+    tile = true, tileSize = 16, edgeSize = 12,
     insets = {left = 2, right = 2, top = 2, bottom = 2}
 })
 MinimapBFBackdrop:SetBackdropColor(0, 0, 0, 1)
 MinimapBFBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
-MinimapBFBackdrop:SetFrameLevel(MinimapContainerBackdrop:GetFrameLevel() + 1)
+MinimapBFBackdrop:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 
 local function MinimapBFUpdate()
     MiniMapBattlefieldBorder:Hide()
-    MiniMapBattlefieldFrame:SetParent(MinimapContainerBackdrop)
+    BattlegroundShine:Hide()
+    MiniMapBattlefieldFrame:SetParent(Minimap)
     MiniMapBattlefieldFrame:ClearAllPoints()
     MiniMapBattlefieldFrame:SetSize(16, 16)
-    MiniMapBattlefieldFrame:SetPoint("LEFT", MinimapTimeBackdrop, "RIGHT", 8, 0)
-
+    MiniMapBattlefieldFrame:SetPoint("LEFT", MinimapTimeBackdrop, "RIGHT", 4, 0)
     MiniMapBattlefieldIcon:ClearAllPoints()
-    MiniMapBattlefieldIcon:SetSize(16,16)
+    MiniMapBattlefieldIcon:SetSize(16, 16)
     MiniMapBattlefieldIcon:SetPoint("CENTER", MiniMapBattlefieldFrame, "CENTER", 0, 0)
 end
 
@@ -158,39 +116,49 @@ MinimapBFEvents:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
 MinimapBFEvents:RegisterEvent("UPDATE_ACTIVE_BATTLEFIELD")
 MinimapBFEvents:SetScript("OnEvent", MinimapBFUpdate)
 
+local function MinimapLFGUpdate()
+    LFGMinimapFrameBorder:Hide()
+    LFGMinimapFrame:SetParent(Minimap)
+    LFGMinimapFrame:ClearAllPoints()
+    LFGMinimapFrame:SetSize(40, 40)
+    LFGMinimapFrame:SetPoint("BOTTOMLEFT", Minimap, "BOTTOMLEFT", -4, 2)
+    LFGMinimapFrameIcon:SetSize(36, 36)
+    LFGMinimapFrameIcon:SetPoint("CENTER", LFGMinimapFrame, "CENTER", 0, 0)
+end
 
+local MinimapLFGEvents = CreateFrame("Frame")
+MinimapLFGEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+MinimapLFGEvents:SetScript("OnEvent", MinimapLFGUpdate)
 
+local MinimapTrackingBackdrop = CreateFrame("Frame", nil, MiniMapTracking, "BackdropTemplate")
+MinimapTrackingBackdrop:SetPoint("TOPLEFT", MiniMapTracking, "TOPLEFT", -4, 4)
+MinimapTrackingBackdrop:SetPoint("BOTTOMRIGHT", MiniMapTracking, "BOTTOMRIGHT", 4, -4)
+MinimapTrackingBackdrop:SetBackdrop({
+    bgFile = "Interface/ChatFrame/ChatFrameBackground",
+    edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+    tile = true, tileSize = 16, edgeSize = 12,
+    insets = {left = 2, right = 2, top = 2, bottom = 2}
+})
+MinimapTrackingBackdrop:SetBackdropColor(0, 0, 0, 1)
+MinimapTrackingBackdrop:SetBackdropBorderColor(0.5, 0.5, 0.5, 1)
+MinimapTrackingBackdrop:SetFrameLevel(Minimap:GetFrameLevel() + 1)
 
 local function MinimapTrackingUpdate()
     MiniMapTrackingBorder:Hide()
-
+    MiniMapTracking:SetParent(Minimap)
     MiniMapTracking:ClearAllPoints()
-    MiniMapTracking:SetSize(16, 16)
-    MiniMapTracking:SetPoint("TOPRIGHT", MinimapContainerBackdrop, "TOPRIGHT", -2, -2)
-
+    MiniMapTracking:SetSize(12, 12)
+    MiniMapTracking:SetPoint("TOP", Minimap, "TOP", 0, 0)
     MiniMapTrackingIcon:ClearAllPoints()
-    MiniMapTrackingIcon:SetSize(16, 16)
-    MiniMapTrackingIcon:SetPoint("TOPRIGHT", MinimapContainerBackdrop, "TOPRIGHT", -2, -2)
-end
-
-local function CheckAndUpdateMinimapTracking()
-    if MiniMapTracking then
-        MinimapTrackingUpdate()
-    else
-        C_Timer.After(1, CheckAndUpdateMinimapTracking)
-    end
+    MiniMapTrackingIcon:SetSize(13, 13)
+    MiniMapTrackingIcon:SetPoint("CENTER", MiniMapTracking, "CENTER", 0, 0)
 end
 
 local MinimapTrackingEvents = CreateFrame("Frame")
-MinimapTrackingEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 MinimapTrackingEvents:RegisterEvent("MINIMAP_UPDATE_TRACKING")
-MinimapTrackingEvents:SetScript("OnEvent", CheckAndUpdateMinimapTracking)
-
-
-
-
-
-
+MinimapTrackingEvents:SetScript("OnEvent", function()
+    C_Timer.After(1, MinimapTrackingUpdate)
+end)
 
 local function AddonButtonUpdate()
     local LibDBIcon = _G.LibStub and _G.LibStub("LibDBIcon-1.0", true)
@@ -206,7 +174,8 @@ local function AddonButtonUpdate()
             end
 
             AddonButton:SetSize(16, 16)
-            AddonButton:SetParent(MinimapContainerBackdrop)
+            AddonButton:SetParent(Minimap)
+            AddonButton:SetFrameLevel(Minimap:GetFrameLevel() + 1)
             AddonButton.icon:ClearAllPoints()
             AddonButton.icon:SetPoint("CENTER", AddonButton, "CENTER", 0, 0)
             AddonButton.icon:SetSize(14, 14)
@@ -216,7 +185,7 @@ local function AddonButtonUpdate()
                 AddonButton.background:SetPoint("TOPLEFT", AddonButton, "TOPLEFT", -4, 4)
                 AddonButton.background:SetPoint("BOTTOMRIGHT", AddonButton, "BOTTOMRIGHT", 4, -4)
                 AddonButton.background:SetBackdrop({
-                    bgFile = "Interface/ChatFrame/ChatFrameBackground",
+                    bgFile = "Interface/ChatFrame/ChatFrameBackground", -- Commented out to prevent black screen
                     edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
                     tile = false, tileSize = 16, edgeSize = 12,
                     insets = {left = 2, right = 2, top = 2, bottom = 2}
