73615c6bfea0eb76a8a1a363ffc290c05bf77a92
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-22 17:38:44 +0100

    Remove unused framerate display update logic from Config module

--- Modules/Config.lua
+++ Modules/Config.lua
@@ -14,18 +14,3 @@ end
 local ConfigEvents = CreateFrame("Frame")
 ConfigEvents:RegisterEvent("PLAYER_LOGIN")
 ConfigEvents:SetScript("OnEvent", ConfigUpdate)
-
-
-
-
--- Update position of framerate display
-
-local function FramerateUpdate()
-    FramerateLabel:SetAlpha(0)
-    FramerateText:ClearAllPoints()
-    FramerateText:SetPoint("LEFT", MainMenuMicroButton, "RIGHT", 8, -8)
-end
-
-local FramerateEvents = CreateFrame("Frame")
-FramerateEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-FramerateEvents:SetScript("OnEvent", FramerateUpdate)
