b5f93de8ed18323c4232bc106704763e67a0b819
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-24 00:37:26 +0100

    Refactor sound muting logic to use a table for sound IDs and improve event handling

--- Modules/Sound.lua
+++ Modules/Sound.lua
@@ -1,17 +1,28 @@
--- Mute bow, gun and Mechastrider mount sounds
+-- Mute bow, gun, and Mechastrider mount sounds
 
 local function SoundUpdate()
-    MuteSoundFile(555124) -- Mechastrider Loop
-    MuteSoundFile(567677) -- Bow Pullback 1
-    MuteSoundFile(567675) -- Bow Pullback 2
-    MuteSoundFile(567676) -- Bow Pullback 3
-    MuteSoundFile(567719) -- Gun Loading 1
-    MuteSoundFile(567720) -- Gun Loading 2
-    MuteSoundFile(567723) -- Gun Loading 3
-
-    MuteSoundFile(548067) -- Core Hound Fire
+    local soundIDsToMute = {
+        567677, -- Bow Pullback 1
+        567675, -- Bow Pullback 2
+        567676, -- Bow Pullback 3
+        567719, -- Gun Loading 1
+        567720, -- Gun Loading 2
+        567723, -- Gun Loading 3
+        555124, -- Mechastrider Loop
+        548067, -- Core Hound Fire Loop
+        567721, -- Gun 1
+        567718, -- Gun 2
+        567722, -- Gun 3
+        567674, -- Bow 1
+        567682  -- Bow 3
+    }
+    for _, soundID in ipairs(soundIDsToMute) do
+        MuteSoundFile(soundID)
+    end
 end
 
-local SoundEvents = CreateFrame("Frame")
-SoundEvents:RegisterEvent("PLAYER_LOGIN")
-SoundEvents:SetScript("OnEvent", SoundUpdate)
+local soundEvents = CreateFrame("Frame")
+soundEvents:RegisterEvent("PLAYER_LOGIN")
+soundEvents:SetScript("OnEvent", function()
+    SoundUpdate()
+end)
