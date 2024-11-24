90b71165a9d897ccff072c6d6adbab43d2de3cb2
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-24 12:42:42 +0100

    Improve LFG minimap frame handling with nil checks for robustness

--- Modules/LFG.lua
+++ Modules/LFG.lua
@@ -1,11 +1,17 @@
 local function MinimapLFGUpdate()
-    LFGMinimapFrameBorder:Hide()
-    LFGMinimapFrame:SetParent(Minimap)
-    LFGMinimapFrame:ClearAllPoints()
-    LFGMinimapFrame:SetSize(44, 44)
-    LFGMinimapFrame:SetPoint("RIGHT", CharacterMicroButton, "LEFT", 0, -8)
-    LFGMinimapFrameIcon:SetSize(40, 40)
-    LFGMinimapFrameIcon:SetPoint("CENTER", LFGMinimapFrame, "CENTER", 0, 0)
+    if LFGMinimapFrameBorder then
+        LFGMinimapFrameBorder:Hide()
+    end
+    if LFGMinimapFrame then
+        LFGMinimapFrame:SetParent(Minimap)
+        LFGMinimapFrame:ClearAllPoints()
+        LFGMinimapFrame:SetSize(44, 44)
+        LFGMinimapFrame:SetPoint("RIGHT", CharacterMicroButton, "LEFT", 0, -8)
+    end
+    if LFGMinimapFrameIcon then
+        LFGMinimapFrameIcon:SetSize(40, 40)
+        LFGMinimapFrameIcon:SetPoint("CENTER", LFGMinimapFrame, "CENTER", 0, 0)
+    end
 end
 
 local MinimapLFGEvents = CreateFrame("Frame")
