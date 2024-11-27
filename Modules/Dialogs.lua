058448a3438302b072ea909cb44a224b824d4a47
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-27 15:32:30 +0100

    Add Dialogs module for enhanced loot and item confirmation handling

---
+++ Modules/Dialogs.lua
@@ -0,0 +1,72 @@
+local frame = CreateFrame("FRAME")
+frame:SetScript("OnEvent", function(self, event, arg1, arg2, ...)
+    if event == "CONFIRM_LOOT_ROLL" then
+        ConfirmLootRoll(arg1, arg2)
+        StaticPopup_Hide("CONFIRM_LOOT_ROLL")
+        return
+    end
+    if event == "LOOT_BIND_CONFIRM" then
+        ConfirmLootSlot(arg1, arg2)
+        StaticPopup_Hide("LOOT_BIND", ...)
+        return
+    end
+    if event == "MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL" then
+        SellCursorItem()
+        return
+    end
+    if event == "MAIL_LOCK_SEND_ITEMS" then
+        RespondMailLockSendItem(arg1, true)
+        return
+    end
+end)
+
+local function SetEvent(enable)
+    if enable then
+        frame:RegisterEvent("CONFIRM_LOOT_ROLL")
+        frame:RegisterEvent("LOOT_BIND_CONFIRM")
+        frame:RegisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
+        frame:RegisterEvent("MAIL_LOCK_SEND_ITEMS")
+    else
+        frame:UnregisterEvent("CONFIRM_LOOT_ROLL")
+        frame:UnregisterEvent("LOOT_BIND_CONFIRM")
+        frame:UnregisterEvent("MERCHANT_CONFIRM_TRADE_TIMER_REMOVAL")
+        frame:UnregisterEvent("MAIL_LOCK_SEND_ITEMS")
+    end
+end
+
+local TypeDeleteLine = gsub(DELETE_GOOD_ITEM, "[\r\n]", "@")
+local void, TypeDeleteLine = strsplit("@", TypeDeleteLine, 2)
+
+RunScript('StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkEnter = function(self, link, text, region, boundsLeft, boundsBottom, boundsWidth, boundsHeight) GameTooltip:SetOwner(self, "ANCHOR_PRESERVE") GameTooltip:ClearAllPoints() local cursorClearance = 30 GameTooltip:SetPoint("TOPLEFT", region, "BOTTOMLEFT", boundsLeft, boundsBottom - cursorClearance) GameTooltip:SetHyperlink(link) end')
+RunScript('StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkLeave = function(self) GameTooltip:Hide() end')
+RunScript('StaticPopupDialogs["DELETE_ITEM"].OnHyperlinkEnter = StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkEnter')
+RunScript('StaticPopupDialogs["DELETE_ITEM"].OnHyperlinkLeave = StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkLeave')
+RunScript('StaticPopupDialogs["DELETE_QUEST_ITEM"].OnHyperlinkEnter = StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkEnter')
+RunScript('StaticPopupDialogs["DELETE_QUEST_ITEM"].OnHyperlinkLeave = StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkLeave')
+RunScript('StaticPopupDialogs["DELETE_GOOD_QUEST_ITEM"].OnHyperlinkEnter = StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkEnter')
+RunScript('StaticPopupDialogs["DELETE_GOOD_QUEST_ITEM"].OnHyperlinkLeave = StaticPopupDialogs["DELETE_GOOD_ITEM"].OnHyperlinkLeave')
+
+local easyDelFrame = CreateFrame("FRAME")
+easyDelFrame:RegisterEvent("DELETE_ITEM_CONFIRM")
+easyDelFrame:SetScript("OnEvent", function()
+    if StaticPopup1EditBox:IsShown() then
+        StaticPopup1:SetHeight(StaticPopup1:GetHeight() - 10)
+        StaticPopup1EditBox:Hide()
+        StaticPopup1Button1:Enable()
+        local link = select(3, GetCursorInfo())
+        if link then
+            StaticPopup1Text:SetText(gsub(StaticPopup1Text:GetText(), gsub(TypeDeleteLine, "@", ""), "") .. "|n" .. link)
+        end
+    else
+        StaticPopup1:SetHeight(StaticPopup1:GetHeight() + 40)
+        StaticPopup1EditBox:Hide()
+        StaticPopup1Button1:Enable()
+        local link = select(3, GetCursorInfo())
+        if link then
+            StaticPopup1Text:SetText(gsub(StaticPopup1Text:GetText(), gsub(TypeDeleteLine, "@", ""), "") .. "|n|n" .. link)
+        end
+    end
+end)
+
+
+
