1ecbad482e89292c34cd60e8d8a07576223735c9
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-18 10:46:03 +0100

    Enhance TargetMacro functionality with support for multiple targets and add clear command

--- Modules/TargetMacro.lua
+++ Modules/TargetMacro.lua
@@ -5,8 +5,6 @@ SlashCmdList["TARGETMACRO"] = function(msg)
     local macroName = "FIND"
     local macroBody
 
-    -- HANDLE NO TARGET CASE
-
     if msg and msg ~= "" then
         macroBody = "/cleartarget\n/target " .. msg
     else
@@ -19,9 +17,7 @@ SlashCmdList["TARGETMACRO"] = function(msg)
         end
     end
 
-    -- CREATE OR UPDATE MACRO IN GENERAL TAB WITH RED ARROW ICON
-
-    macroBody = macroBody .. "\n/run local m=8; if UnitExists(\"target\") and GetRaidTargetIndex(\"target\")~=m then SetRaidTarget(\"target\",m) end"
+    macroBody = macroBody .. "\n/run if UnitExists(\"target\") and not GetRaidTargetIndex(\"target\") then SetRaidTarget(\"target\",8) end"
 
     local macroIndex = GetMacroIndexByName(macroName)
     if macroIndex > 0 then
@@ -38,12 +34,11 @@ SlashCmdList["TARGETMACRO"] = function(msg)
     print("Macro '" .. macroName .. "' has been updated with " .. table.concat(targets, ", ") .. ".")
 end
 
--- REGISTER SLASH COMMAND FOR ADDING TARGETS
-
 SLASH_TARGETMACROADD1 = "/fm+"
 SlashCmdList["TARGETMACROADD"] = function(msg)
     local macroName = "FIND"
-    local macroBody = GetMacroBody(macroName) or ""
+    local macroIndex = GetMacroIndexByName(macroName)
+    local macroBody = macroIndex > 0 and GetMacroBody(macroName) or ""
     local targetName
 
     if msg and msg ~= "" then
@@ -56,18 +51,43 @@ SlashCmdList["TARGETMACROADD"] = function(msg)
         end
     end
 
-    local count = select(2, macroBody:gsub("/target", ""))
-    if count >= 3 then
-        print("Cannot add more than 3 targets.")
+    -- Extract existing targets
+    local existingTargets = {}
+    for target in macroBody:gmatch("/target ([^\n]+)") do
+        table.insert(existingTargets, target)
+    end
+
+    -- Check for duplicates
+    for _, target in ipairs(existingTargets) do
+        if target == targetName then
+            print(targetName .. " is already in the target list.")
+            return
+        end
+    end
+
+    if #existingTargets >= 2 then
+        print("Cannot add more than 2 targets. Use '/fmclear' to reset the list.")
         return
     end
 
-    macroBody = macroBody:gsub("\n/run .+", "") -- Remove existing run command
-    macroBody = macroBody .. "\n/target " .. targetName
+    if macroBody == "" or not macroBody:find("/cleartarget") then
+        macroBody = "/cleartarget\n/target " .. targetName
+        macroBody = macroBody .. "\n/run if UnitExists(\"target\") and not GetRaidTargetIndex(\"target\") then SetRaidTarget(\"target\",8) end"
+    else
+        macroBody = macroBody:gsub("\n/run .+", "")
+        macroBody = macroBody .. "\n/target " .. targetName
+        
+        local markerScript = "\n/run "
+        markerScript = markerScript .. "local t1,t2=UnitName(\"target\"),\"\"; "
+        markerScript = markerScript .. "TargetLastTarget(); "
+        markerScript = markerScript .. "if UnitExists(\"target\") then t2=UnitName(\"target\"); end; "
+        markerScript = markerScript .. "TargetByName(t1); "
+        markerScript = markerScript .. "if UnitExists(\"target\") and not GetRaidTargetIndex(\"target\") then SetRaidTarget(\"target\",8); end; "
+        markerScript = markerScript .. "if t2~=\"\" then TargetByName(t2); if UnitExists(\"target\") and not GetRaidTargetIndex(\"target\") then SetRaidTarget(\"target\",7); end; end"
+        
+        macroBody = macroBody .. markerScript
+    end
 
-    macroBody = macroBody .. "\n/run local m=8; if UnitExists(\"target\") and GetRaidTargetIndex(\"target\")~=m then SetRaidTarget(\"target\",m) end"
-
-    local macroIndex = GetMacroIndexByName(macroName)
     if macroIndex > 0 then
         EditMacro(macroIndex, macroName, "Ability_Hunter_SniperShot", macroBody)
     else
@@ -80,4 +100,49 @@ SlashCmdList["TARGETMACROADD"] = function(msg)
     end
 
     print("Macro '" .. macroName .. "' has been updated with " .. table.concat(targets, ", ") .. ".")
+end
+
+SLASH_TARGETMACROCLEAR1 = "/fmclear"
+SlashCmdList["TARGETMACROCLEAR"] = function(msg)
+    local macroName = "FIND"
+    local macroIndex = GetMacroIndexByName(macroName)
+    
+    if macroIndex > 0 then
+        DeleteMacro(macroIndex)
+        print("Macro '" .. macroName .. "' has been cleared.")
+    else
+        print("Macro '" .. macroName .. "' doesn't exist.")
+    end
+end
+
+
+-- CREATE ADDITIONAL MACRO FOR SECOND TARGET
+
+SLASH_TARGETMACROSECOND1 = "/fm2"
+SlashCmdList["TARGETMACROSECOND"] = function(msg)
+    local macroName = "FIND2"
+    local macroBody
+
+    if msg and msg ~= "" then
+        macroBody = "/cleartarget\n/target " .. msg
+    else
+        local targetName = UnitName("target")
+        if targetName then
+            macroBody = "/cleartarget\n/target " .. targetName
+        else
+            print("No target selected and no name provided.")
+            return
+        end
+    end
+
+    macroBody = macroBody .. "\n/run if UnitExists(\"target\") and not GetRaidTargetIndex(\"target\") then SetRaidTarget(\"target\",7) end"
+
+    local macroIndex = GetMacroIndexByName(macroName)
+    if macroIndex > 0 then
+        EditMacro(macroIndex, macroName, "Ability_Hunter_SniperShot", macroBody)
+    else
+        CreateMacro(macroName, "Ability_Hunter_SniperShot", macroBody, nil)
+    end
+
+    print("Macro '" .. macroName .. "' has been updated with cross marker.")
 end
