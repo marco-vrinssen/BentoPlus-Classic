d51506d8921857fb2f768d6a08f7be56a6a6bb44
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-18 21:54:45 +0100

    Update graphics configuration settings for improved visuals and clarity

--- Modules/Graphics.lua
+++ Modules/Graphics.lua
@@ -1,12 +1,21 @@
 -- UPDATE GRAPHICS CONFIG
 
 local function updateCVars()
+    SetCVar("cameraDistanceMaxZoomFactor", 2.4)
+    SetCVar("HardwareCursor", 1)
+
     SetCVar("ffxGlow", 0)
     SetCVar("ffxDeath", 0)
     SetCVar("ffxNether", 0)
-    SetCVar("WorldTextScale", 1.25)
-    SetCVar("cameraDistanceMaxZoomFactor", 2.2)
-    SetCVar("HardwareCursor", 1)
+
+    SetCVar("WorldTextScale", 1.5)
+
+    SetCVar("UnitNameNPC", 1)
+    SetCVar("UnitNameHostleNPC", 0)
+    SetCVar("UnitNameInteractiveNPC", 0)
+
+    SetCVar("UnitNamePlayerGuild", 1)
+    SetCVar("UnitNamePlayerPVPTitle", 0)
 end
 
 local graphicsFrame = CreateFrame("Frame")
