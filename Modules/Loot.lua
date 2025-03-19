4154504747a33af1f2368f3a47682332940d922b
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-19 15:50:17 +0100

    Improve auto-looting functionality to handle failed loot attempts and show loot frame when necessary

--- Modules/Loot.lua
+++ Modules/Loot.lua
@@ -3,16 +3,28 @@
 local lootStart = 0
 local lootDelay = 0.1
 
+
+-- AUTO LOOTING FUNCTION
+
 local function autoLooting()
 	if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
 		if (GetTime() - lootStart) >= lootDelay then
+			local allLooted = true
 			for i = GetNumLootItems(), 1, -1 do
-				LootSlot(i)
+				if not LootSlot(i) then
+					allLooted = false
+				end
+			end
+			if not allLooted then
+				LootFrame:Show()
 			end
 			lootStart = GetTime()
 		end
 	end
 end
+
+
+-- REGISTER LOOT EVENTS
 
 local fasterLootEvents = CreateFrame("Frame")
 fasterLootEvents:RegisterEvent("LOOT_READY")
