5976a1110e345fc3ebf068b105bc366d08eb22ad
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-20 17:14:58 +0100

    Refactor auto-looting functionality for improved efficiency and clarity

--- Modules/Loot.lua
+++ Modules/Loot.lua
@@ -1,31 +1,19 @@
--- FASTER AUTO LOOTING
+-- AUTO LOOT ITEMS WHEN LOOT WINDOW OPENS
 
-local lootStart = 0
-local lootDelay = 0.1
-
-
--- AUTO LOOTING FUNCTION
+local lastLootTime = 0
+local LOOT_DELAY = 0.1
 
 local function autoLooting()
-	if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
-		if (GetTime() - lootStart) >= lootDelay then
-			local allLooted = true
-			for i = GetNumLootItems(), 1, -1 do
-				if not LootSlot(i) then
-					allLooted = false
-				end
-			end
-			if not allLooted then
-				LootFrame:Show()
-			end
-			lootStart = GetTime()
-		end
-	end
+    if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+        if (GetTime() - lastLootTime) >= LOOT_DELAY then
+            for i = GetNumLootItems(), 1, -1 do
+                LootSlot(i)
+            end
+            lastLootTime = GetTime()
+        end
+    end
 end
 
-
--- REGISTER LOOT EVENTS
-
-local fasterLootEvents = CreateFrame("Frame")
-fasterLootEvents:RegisterEvent("LOOT_READY")
-fasterLootEvents:SetScript("OnEvent", autoLooting)
+local autoLootEvents = CreateFrame("Frame")
+autoLootEvents:RegisterEvent("LOOT_READY")
+autoLootEvents:SetScript("OnEvent", autoLooting)
