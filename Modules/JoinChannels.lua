e1d70957a84069a452950156e6ab559597b9d9fe
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-03-27 10:10:29 +0100

    Refactor auto-join functionality to join channels on PLAYER_ENTERING_WORLD event

--- Modules/JoinChannels.lua
+++ Modules/JoinChannels.lua
@@ -3,6 +3,8 @@
 local function autoJoinChannels()
     local channels = { "World", "LookingForGroup" }
     local channelList = { GetChannelList() }
+    local channelsToJoin = {}
+
     for _, channel in ipairs(channels) do
         local alreadyInChannel = false
         for i = 1, #channelList, 3 do
@@ -12,11 +14,17 @@ local function autoJoinChannels()
             end
         end
         if not alreadyInChannel then
+            table.insert(channelsToJoin, channel)
+        end
+    end
+
+    if #channelsToJoin > 0 then
+        for _, channel in ipairs(channelsToJoin) do
             JoinChannelByName(channel)
         end
     end
 end
 
 local channelEvents = CreateFrame("Frame")
-channelEvents:RegisterEvent("PLAYER_LOGIN")
+channelEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 channelEvents:SetScript("OnEvent", autoJoinChannels)
